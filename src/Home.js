f2a5d0b45ae76b2a66d868d7a801033c0998ce6b
ThomasYap <thomasyap@seekasia.com>
2020-10-04 13:11:26 +0800

    fix home page

--- src/Home.js
+++ src/Home.js
@@ -3,27 +3,43 @@ import {
   withRouter
 } from "react-router-dom";
 import { Button } from '@material-ui/core';
+import home from './images/home.png'
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
 
 import './App.css';
 
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    height: '600px'
+  },
+  img: {
+    margin: "auto",
+    display: "block",
+    maxWidth: "100%",
+    maxHeight: "100%",
+  },
+}));
+
 function Home(props) {
-  
+  const classes = useStyles();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <Button variant="contained" color="primary"
-         onClick={() => props.history.push('/nasa') }>
-          Nasa Engineer
-        </Button>
-
-        <br/>
+        <Container className={classes.container}>
+          <img className={classes.img} alt="home" src={home} />
+        </Container>
 
         <Button variant="contained" color="primary"
          onClick={() => props.history.push('/kid') }>
-          Kid
+          Become an engineer
         </Button>
-      </header>
-
+        <br/>   <br/>
+        <Button color="primary"
+         onClick={() => props.history.push('/nasa') }>
+          Log in as Nasa Engineer
+        </Button>
     </div>
   );
 }
