1c272af4af1d218a2fdee48143288c380509f629
Thomas Yap Hock Toh <yhtthomas@gmail.com>
2021-05-30 20:04:56 +0800

    change

--- src/Achievement.js
+++ src/Achievement.js
@@ -22,7 +22,7 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     padding: theme.spacing(2),
     margin: "auto",
-    maxWidth: 500,
+    maxWidth: 600,
   },
   img: {
     margin: "auto",
